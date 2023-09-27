7eb881e8650c19e5bb215356af77fa3c1d6a37e2
Mariana <mariananavarro3022@gmail.com>
2023-09-26 23:54:06 -0600

    Body

--- Scripts/Packages/#1.1_UserPackage_Body.sql
+++ Scripts/Packages/#1.1_UserPackage_Body.sql
@@ -46,9 +46,71 @@ CREATE SEQUENCE sCanton
   NOCACHE
   CYCLE;
   
+CREATE SEQUENCE sTelephone
+  START WITH 000001
+  INCREMENT BY 1
+  MINVALUE 000001
+  MAXVALUE 999999
+  NOCACHE
+  CYCLE;
+  
+  CREATE SEQUENCE sEmail
+  START WITH 000001
+  INCREMENT BY 1
+  MINVALUE 000001
+  MAXVALUE 999999
+  NOCACHE
+  CYCLE;
+  
+CREATE SEQUENCE sUser
+  START WITH 000001
+  INCREMENT BY 1
+  MINVALUE 000001
+  MAXVALUE 999999
+  NOCACHE
+  CYCLE;
+  
+CREATE SEQUENCE sUserPassword
+  START WITH 000001
+  INCREMENT BY 1
+  MINVALUE 000001
+  MAXVALUE 999999
+  NOCACHE
+  CYCLE;
+  
+CREATE SEQUENCE sPhotoStay
+  START WITH 000001
+  INCREMENT BY 1
+  MINVALUE 000001
+  MAXVALUE 999999
+  NOCACHE
+  CYCLE;
+  CREATE SEQUENCE sPhotoSleep
+  START WITH 000001
+  INCREMENT BY 1
+  MINVALUE 000001
+  MAXVALUE 999999
+  NOCACHE
+  CYCLE;
+  CREATE SEQUENCE sOtherPet
+  START WITH 000001
+  INCREMENT BY 1
+  MINVALUE 000001
+  MAXVALUE 999999
+  NOCACHE
+  CYCLE;
+  CREATE SEQUENCE sPhotoOP
+  START WITH 000001
+  INCREMENT BY 1
+  MINVALUE 000001
+  MAXVALUE 999999
+  NOCACHE
+  CYCLE;
+  
 CREATE OR REPLACE PACKAGE BODY createUser AS
 
------INCERT PERSON-----
+-------------------------
+------INCERT PERSON------
 PROCEDURE insertPerson (pcDistrictN VARCHAR2, pcCantonN VARCHAR2,
           pcProvinceN VARCHAR2, pcCountryN VARCHAR2, pcContinentN VARCHAR2, 
           pcFirstN VARCHAR2, pcMiddleN VARCHAR2, pcFirstLastN VARCHAR2, 
@@ -57,26 +119,77 @@ IS
 BEGIN
     INSERT INTO person(id, first_name, middle_name, first_last_name, second_last_name)
     VALUES (sPerson.NEXTVAL(),pcFirstN, pcMiddleN, pcFirstLastN, pcFirstLastN);
-  
+  --
     INSERT INTO Continent(id, continent_name)
     VALUES (sContinent.NEXTVAL, pcContinentN);
-    
+  --  
     INSERT INTO Country(id, id_continent, country_name)
     VALUES (sCountry.NEXTVAL,sContinent.CURRVAL, pcCountryN);
-    
+  --  
     INSERT INTO province(id, id_country, province_name)
     VALUES (sProvince.NEXTVAL,sCountry.CURRVAL, pcProvinceN);
-    
+  --  
     INSERT INTO canton(id, id_province, canton_name)
     VALUES (sCanton.NEXTVAL,sProvince.CURRVAL, pcCantonN);
-    
+  --  
     INSERT INTO District(id, id_canton, district_name)
     VALUES (sDistrict.CURRVAL, sCanton.CURRVAL, pcDistrictN);
-    
+  --  
     INSERT INTO personxDistrict(id_person, id_district)
     VALUES (sPerson.CURRVAL, sDistrict.CURRVAL);
+    COMMIT;      
+END;
+-------------------------
+-----INCERT TELEPHONE----
+PROCEDURE insertTelephone(pnIdPerson NUMBER, pnPhoneNumber NUMBER)
+IS
+BEGIN
+    INSERT INTO telephone(id,phone_number)
+    VALUES (sTelephone.NEXTVAL, pnPhoneNumber);
+    
+    INSERT INTO telephoneXperson(id_person, id_telephone)
+    VALUES (pnIdPerson, sTelephone.CURRVAL);
     COMMIT;
 END;
---------------------
+-------------------------
+-------INCERT EMAIL------
+PROCEDURE insertEmail(pnIdPerson NUMBER, pcEmailText VARCHAR2)
+IS
+BEGIN
+    INSERT INTO email(id,id_person,email_text)
+    VALUES (sEmail.NEXTVAL, pnIdPerson,pcEmailText);
+    COMMIT;
+END;
+-------------------------
+-------CREATE USER-------
+PROCEDURE createUser(pnIdPerson NUMBER, pcName VARCHAR2, pcPassword VARCHAR2)
+IS
+BEGIN
+    INSERT INTO user_person(id, id_person,username)
+    VALUES (sUser.NEXTVAL, pnIdPerson, pcName);
+    
+    INSERT INTO user_password(id, id_user,name_type)
+    VALUES (sUserPassword.NEXTVAL, sUser.CURRVAL, pcPassword);
+    COMMIT;
+END;
+-------------------------
+-------PHOTO STAY--------
+PROCEDURE incertPhotoStay(pnIdPerson NUMBER, pcPhotoPath VARCHAR2)
+IS
+BEGIN 
+    INSERT INTO photo_stay(id, id_candidate,pitcutre_path)
+    VALUES (sPhotoStay.NEXTVAL, pnIdPerson, pcPhotoPath);
+    COMMIT;
+END;
+-------------------------
+------PHOTO SLEEP--------
+PROCEDURE incertPhotoSleep(pnIdPerson NUMBER, pcPhotoPath VARCHAR2)
+IS
+BEGIN 
+    INSERT INTO photo_sleep(id, id_candidate,pitcutre_path)
+    VALUES (sPhotoSleep.NEXTVAL, pnIdPerson, pcPhotoPath);
+    COMMIT;
+END;
+-------------------------
 
 END createUser;
--- Scripts/Packages/#1UserPackage.sql
+++ Scripts/Packages/#1UserPackage.sql
@@ -7,15 +7,15 @@ PROCEDURE insertPerson (pcDistrictN VARCHAR2, pcCantonN VARCHAR2,
           pcSecondLastN VARCHAR2);
 PROCEDURE insertTelephone(pnIdPerson NUMBER, pnPhoneNumber NUMBER);
 PROCEDURE insertEmail(pnIdPerson NUMBER, pcEmailText VARCHAR2);
-PROCEDURE createUser(pcName VARCHAR2, pcPassword VARCHAR2);         
+PROCEDURE createUser(pnIdPerson NUMBER, pcName VARCHAR2, pcPassword VARCHAR2);         
 
 -- FORMS PROCEDURES
 PROCEDURE answerAdoptionForm(pnIdPerson NUMBER, pcOwnHouse VARCHAR, 
           pcAuthorization VARCHAR, pcPurpose VARCHAR,pcInterestAdoption VARCHAR, 
-          pcAccompainmentA VARCHAR, pcMinimumMA VARCHAR, pcMaximumMA VARCHAR);
+          pcAccompainmentA VARCHAR, pcMinimumMA VARCHAR, pcMaximumMA VARCHAR); ---- NO SE COMO HACER ESTE
 PROCEDURE incertPhotoStay(pnIdPerson NUMBER, pcPhotoPath VARCHAR2);
 PROCEDURE incertPhotoSleep(pnIdPerson NUMBER, pcPhotoPath VARCHAR2);
-PROCEDURE incertOtherPet(pnIdPerson NUMBER, pnYearAdopted VARCHAR2);
+PROCEDURE incertOtherPet(pnIdPerson NUMBER, pnYearAdopted NUMBER);
 PROCEDURE incertPhotoOP(pnIdPerson NUMBER, pcPhotoPath VARCHAR2);
           
 END createUser;
