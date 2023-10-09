22e26b8969c44cb0624c52ae841920897d1e2306
Mariana <mariananavarro3022@gmail.com>
2023-10-08 20:39:43 -0600

    vuelvo a subir

--- Scripts/Packages/#1.1_UserPackage_Body.sql
+++ Scripts/Packages/#1.1_UserPackage_Body.sql
@@ -1,34 +1,5 @@
-CREATE OR REPLACE PACKAGE createUser IS
-PROCEDURE insertPerson (pcDistrictN VARCHAR2, pcCantonN VARCHAR2,
-          pcProvinceN VARCHAR2, pcCountryN VARCHAR2, pcContinentN VARCHAR2, 
-          pcFirstN VARCHAR2, pcMiddleN VARCHAR2, pcFirstLastN VARCHAR2, 
-          pcSecondLastN VARCHAR2); 
-          
-PROCEDURE insertTelephone(pnIdPerson NUMBER, pnPhoneNumber NUMBER); 
-
-PROCEDURE insertEmail(pnIdPerson NUMBER, pcEmailText VARCHAR2); 
-
-PROCEDURE createUser(pnIdPerson NUMBER, pcName VARCHAR2, pcPassword VARCHAR2); 
-
-PROCEDURE answerCandidate(pnIdPerson NUMBER, pcOwnHouse VARCHAR, 
-          pcAuthorization VARCHAR, pcPurpose VARCHAR,pcInterestAdoption VARCHAR, 
-          pcAccompainmentA VARCHAR, pcMinimumMA VARCHAR, pcMaximumMA VARCHAR,
-          pnIdTest NUMBER); 
-
-PROCEDURE insertPhotoStay(pnIdPerson NUMBER, pcPhotoPath VARCHAR2); 
-
-PROCEDURE insertPhotoSleep(pnIdPerson NUMBER, pcPhotoPath VARCHAR2); 
-
-PROCEDURE insertOtherPet(pnIdPerson NUMBER, pnYearAdopted DATE); 
-
-PROCEDURE insertPhotoOP(pnIdOtherpet NUMBER, pcPhotoPath VARCHAR2); 
-
-END createUser; 
-
------------------------------------------------------------------=================
-
+-------------------------
 CREATE OR REPLACE PACKAGE BODY createUser AS
-
 -------------------------
 ------INSERT PERSON------
 PROCEDURE insertPerson (pcDistrictN VARCHAR2, pcCantonN VARCHAR2,
--- Scripts/Packages/#1UserPackage.sql
+++ Scripts/Packages/#1UserPackage.sql
@@ -5,6 +5,7 @@ PROCEDURE insertPerson (pcDistrictN VARCHAR2, pcCantonN VARCHAR2,
           pcProvinceN VARCHAR2, pcCountryN VARCHAR2, pcContinentN VARCHAR2, 
           pcFirstN VARCHAR2, pcMiddleN VARCHAR2, pcFirstLastN VARCHAR2, 
           pcSecondLastN VARCHAR2);
+          
 PROCEDURE insertTelephone(pnIdPerson NUMBER, pnPhoneNumber NUMBER);
 PROCEDURE insertEmail(pnIdPerson NUMBER, pcEmailText VARCHAR2);
 PROCEDURE createUser(pnIdPerson NUMBER, pcName VARCHAR2, pcPassword VARCHAR2);         
@@ -14,9 +15,9 @@ PROCEDURE answerCandidate(pnIdPerson NUMBER, pcOwnHouse VARCHAR,
           pcAuthorization VARCHAR, pcPurpose VARCHAR,pcInterestAdoption VARCHAR, 
           pcAccompainmentA VARCHAR, pcMinimumMA VARCHAR, pcMaximumMA VARCHAR,
           pnIdTest NUMBER); ---- NO SE COMO HACER ESTE
-PROCEDURE incertPhotoStay(pnIdPerson NUMBER, pcPhotoPath VARCHAR2);
-PROCEDURE incertPhotoSleep(pnIdPerson NUMBER, pcPhotoPath VARCHAR2);
-PROCEDURE incertOtherPet(pnIdPerson NUMBER, pnYearAdopted DATE);
-PROCEDURE incertPhotoOP(pnIdOtherpet NUMBER, pcPhotoPath VARCHAR2);
+PROCEDURE insertPhotoStay(pnIdPerson NUMBER, pcPhotoPath VARCHAR2);
+PROCEDURE insertPhotoSleep(pnIdPerson NUMBER, pcPhotoPath VARCHAR2);
+PROCEDURE insertOtherPet(pnIdPerson NUMBER, pnYearAdopted DATE);
+PROCEDURE insertPhotoOP(pnIdOtherpet NUMBER, pcPhotoPath VARCHAR2);
           
 END createUser;
